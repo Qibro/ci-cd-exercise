902f4e8a19e68123f4910408559654e345fd3450
Qibro <muhamad.ibrahim21@gmail.com>
2020-10-31 04:26:21 +0700

    testing

--- public/index.php
+++ public/index.php
@@ -48,6 +48,7 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 | and wonderful application we have prepared for them.
 |
 */
+$omegaLul = "huhwut";
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
